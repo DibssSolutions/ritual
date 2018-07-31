0c348a0f7f340435d197508e3e5263ee889762bc
Stas Umanskiy <stasumanskiy@gmail.com>
2018-07-31 18:24:17 +0300

    fix

--- src/js/components/_init-fancybox.js
+++ src/js/components/_init-fancybox.js
@@ -1,13 +1,5 @@
 import fancybox from '@fancyapps/fancybox';
 
-// const container = $('.js-fancybox');
-// container.fancybox({
-//   protect 		  : true,
-//   keyboard        : true,
-//   animationEffect : false,
-//   arrows          : true,
-//   clickContent    : false
-// });
 
 $('[data-fancybox]').fancybox({
   // Options will go here
