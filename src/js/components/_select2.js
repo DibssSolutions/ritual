4494c56e4e523347cee2de926763c2cab1f75c45
Roman Muratov <myratov.r@gmail.com>
2018-08-15 17:27:28 +0300

    fixed bugs

--- src/js/components/_select2.js
+++ src/js/components/_select2.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
   $('.js-select').select2({
     minimumResultsForSearch: Infinity
   });
+
+  $('.ps-select select').select2({
+    minimumResultsForSearch: Infinity
+  });
+  // $($('.ps-select select').data('select2').$container).addClass('select--small');
+  // $($('.ps-select select').data('select2').$dropdown).addClass('select--small__drop');
+  $('.ps-select select').each(function() {
+    $($(this).data('select2').$container).addClass('select--small');
+    $($(this).data('select2').$dropdown).addClass('select--small__drop');
+  });
 });
